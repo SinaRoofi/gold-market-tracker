411c937ad24ee18d3eacf834acd4338d1b1efde2
SinaRoufi <103225253+SinaRoofi@users.noreply.github.com>
2025-11-15 21:21:38 +0330

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,19 +1,18 @@
-"""
-Utils package for Gold Market Tracker
-"""
-
 from .data_fetcher import (
-    fetch_gold_price,
+    fetch_gold_price_today,
+    fetch_gold_price_yesterday,
     fetch_dollar_prices,
     fetch_yesterday_close,
     fetch_market_data,
 )
+
 from .data_processor import process_market_data
 from .telegram_sender import send_to_telegram
 from .holidays import is_iranian_holiday, is_working_day
 
 __all__ = [
-    "fetch_gold_price",
+    "fetch_gold_price_today",
+    "fetch_gold_price_yesterday",
     "fetch_dollar_prices",
     "fetch_yesterday_close",
     "fetch_market_data",
