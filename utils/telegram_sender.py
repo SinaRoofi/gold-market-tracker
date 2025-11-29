58ae9445fa6e1c7b76efbf504f9df485b48223f4
SinaRoufi <103225253+SinaRoofi@users.noreply.github.com>
2025-11-29 14:44:06 +0330

    Update telegram_sender.py

--- utils/telegram_sender.py
+++ utils/telegram_sender.py
@@ -493,7 +493,7 @@ def create_simple_caption(data, dollar_prices, gold_price, gold_yesterday,
 💰 ارزش معاملات: {total_value:,.0f} م.ت ({value_to_avg_ratio:.0f}%)
 💸 پول حقیقی: {total_pol:+,.0f} م.ت ({pol_to_value_ratio:+.0f}%)
 📈 آخرین قیمت: {avg_price_weighted:,.0f} ({avg_change_percent_weighted:+.2f}%)
-📊 NAV: {avg_nav_weighted:,.0f} ({avg_nav_change_weighted:+.2f}%)
+🔹 NAV: {avg_nav_weighted:,.0f} ({avg_nav_change_weighted:+.2f}%)
 🎈 میانگین حباب: {avg_bubble_weighted:+.2f}%
 ━━━━━━━━━━━━━━━━━━━━━━━━
 ✨ شمش طلا
