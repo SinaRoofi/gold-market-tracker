736433a8969268d637e3ca2b21ffaa7b093b37f6
SinaRoufi <103225253+SinaRoofi@users.noreply.github.com>
2025-12-15 13:22:35 +0330

    Delete utils/datetime_utils.py

--- utils/datetime_utils.py
+++
@@ -1,9 +0,0 @@
-import pytz
-import jdatetime
-from datetime import datetime
-from config import TIMEZONE
-
-def get_jalali_now_str(fmt='%Y-%m-%d - %H:%M'):
-    tz = pytz.timezone(TIMEZONE)
-    now = datetime.now(tz)
-    return jdatetime.datetime.fromgregorian(datetime=now).strftime(fmt)
